29908764c5d1a688c515dc9b124c9f8f728606bd
Yash Agarwal <yagarwal1307@gmail.com>
2025-10-07 21:30:21 +0800

    chore: make changes to make the china cloud work

--- cloud/packages/cloud/src/connections/mongodb.connection.ts
+++ cloud/packages/cloud/src/connections/mongodb.connection.ts
@@ -1,26 +1,25 @@
 import { logger } from "../services/logging/pino-logger";
 import dotenv from "dotenv";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 dotenv.config();
 const MONGO_URL: string | undefined = process.env.MONGO_URL;
-const NODE_ENV: string | undefined = process.env.NODE_ENV;
-const IS_PROD = NODE_ENV === 'production';
+// const NODE_ENV: string | undefined = process.env.NODE_ENV;
+// const IS_PROD = NODE_ENV === 'production';
 
 // Connect to mongo db.
 export async function init(): Promise<void> {
   if (!MONGO_URL) throw "MONGO_URL is undefined";
   try {
-    mongoose.set('strictQuery', false);
-    const database = IS_PROD ? 'prod' : 'dev';
+    mongoose.set("strictQuery", false);
+    // const database = IS_PROD ? 'prod' : 'dev';
 
-    await mongoose.connect(MONGO_URL + "/prod");
+    await mongoose.connect(MONGO_URL);
     // After connection
-    await mongoose.connection.db.collection('test').insertOne({ test: true });
+    await mongoose.connection.db.collection("test").insertOne({ test: true });
 
-    logger.info('Mongoose Connected');
-  }
-  catch (error) {
+    logger.info("Mongoose Connected");
+  } catch (error) {
     logger.error(`Unable to connect to database(${MONGO_URL}) ${error}`);
     throw error;
   }
