bf2f15eff61e8697ab377c8f16100582aec026ec
fossephate <6175367+fossephate@users.noreply.github.com>
2025-10-14 15:35:57 -0700

    cleanup

--- mobile/src/utils/NotificationServiceUtils.tsx
+++ mobile/src/utils/NotificationServiceUtils.tsx
@@ -1,4 +1,4 @@
-import {Alert, NativeModules, Platform} from "react-native"
+import {NativeModules, Platform} from "react-native"
 import showAlert from "./AlertUtils"
 
 const {NotificationAccess} = NativeModules
