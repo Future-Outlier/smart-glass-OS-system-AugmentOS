c68ecd37405e04a78e0fa105196a5afb596dbc75
fossephate <6175367+fossephate@users.noreply.github.com>
2025-10-21 16:45:15 -0700

    custom-tab-bar

--- mobile/src/app/(tabs)/_layout.tsx
+++ mobile/src/app/(tabs)/_layout.tsx
@@ -1,194 +1,106 @@
-import {useRef} from "react"
-import {Tabs} from "expo-router/tabs"
+import {Text} from "@/components/ignite"
 import {translate} from "@/i18n"
 import {ThemedStyle} from "@/theme"
-import {TextStyle, TouchableOpacity, View, ViewStyle} from "react-native"
-import {useSafeAreaInsets} from "react-native-safe-area-context"
 import {useAppTheme} from "@/utils/useAppTheme"
-import {LinearGradient} from "expo-linear-gradient"
 import SolarLineIconsSet4 from "assets/icons/component/SolarLineIconsSet4"
 import HomeIcon from "assets/icons/navbar/HomeIcon"
 import StoreIcon from "assets/icons/navbar/StoreIcon"
 import UserIcon from "assets/icons/navbar/UserIcon"
-import showAlert from "@/utils/AlertUtils"
-import Toast from "react-native-toast-message"
-import {useNavigationHistory} from "@/contexts/NavigationHistoryContext"
-import {SETTINGS_KEYS} from "@/stores/settings"
-import {useSettingsStore} from "@/stores/settings"
-import {Platform} from "react-native"
-import {NativeTabs, Icon, Label} from "expo-router/unstable-native-tabs"
+import {TabList, Tabs, TabSlot, TabTrigger, TabTriggerSlotProps} from "expo-router/ui"
+import {Pressable, TextStyle, ViewStyle} from "react-native"
+import {useSafeAreaInsets} from "react-native-safe-area-context"
+
+type TabButtonProps = TabTriggerSlotProps & {
+  icon: React.ComponentType<{size: number; color: string}>
+  label: string
+}
 
 export default function Layout() {
   const {theme, themed} = useAppTheme()
-  const {replace} = useNavigationHistory()
+  const {bottom} = useSafeAreaInsets()
 
-  const iconFocusedColor = theme.colors.primary
-  const iconInactiveColor = theme.colors.textDim
-
-  if (Platform.OS === "ios") {
+  function TabButton({icon: Icon, isFocused, label, ...props}: TabButtonProps) {
+    const iconColor = isFocused ? theme.colors.primary : theme.colors.textDim
+    const textColor = isFocused ? theme.colors.text : theme.colors.textDim
     return (
-      <NativeTabs backgroundColor={"#FFFFFF"}>
-        <NativeTabs.Trigger name="home">
-          <Label>{translate("navigation:home")}</Label>
-          <Icon sf="house.fill" drawable="custom_android_drawable" />
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="glasses">
-          <Icon sf="eyes" drawable="custom_settings_drawable" />
-          <Label>{translate("navigation:glasses")}</Label>
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="store">
-          <Icon sf="cart.fill" drawable="custom_settings_drawable" />
-          <Label>{translate("navigation:store")}</Label>
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="settings">
-          <Icon sf="gear" drawable="custom_settings_drawable" />
-          <Label>{translate("navigation:account")}</Label>
-        </NativeTabs.Trigger>
-      </NativeTabs>
+      <Pressable {...props} style={[themed($tabButton), {marginBottom: bottom}]}>
+        <Icon size={28} color={iconColor} />
+        <Text text={label} style={[themed($tabLabel), {color: textColor}]} />
+      </Pressable>
     )
   }
 
+  // if (Platform.OS === "ios") {
+  //   return (
+  //     <NativeTabs minimizeBehavior="onScrollDown" disableTransparentOnScrollEdge>
+  //       <NativeTabs.Trigger name="home">
+  //         <Label>{translate("navigation:home")}</Label>
+  //         <Icon sf="house.fill" drawable="custom_android_drawable" />
+  //       </NativeTabs.Trigger>
+  //       <NativeTabs.Trigger name="glasses">
+  //         <Icon sf="sunglasses.fill" drawable="custom_settings_drawable" />
+  //         <Label>{translate("navigation:glasses")}</Label>
+  //       </NativeTabs.Trigger>
+  //       <NativeTabs.Trigger name="store">
+  //         <Icon sf="cart.fill" drawable="custom_settings_drawable" />
+  //         <Label>{translate("navigation:store")}</Label>
+  //       </NativeTabs.Trigger>
+  //       <NativeTabs.Trigger name="settings">
+  //         <Icon sf="gear" drawable="custom_settings_drawable" />
+  //         <Label>{translate("navigation:account")}</Label>
+  //       </NativeTabs.Trigger>
+  //     </NativeTabs>
+  //   )
+  // }
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarHideOnKeyboard: true,
-        tabBarShowLabel: true,
-        tabBarStyle: [
-          themed($tabBar),
-          {
-            // height: 90,
-            // paddingBottom: 10,
-            // marginBottom: 100,
-            // borderTopColor moved to View wrapping LinearGradient
-            borderTopWidth: 0,
-            // backgroundColor: "transparent",
-          },
-        ],
-        tabBarActiveTintColor: iconFocusedColor,
-        tabBarInactiveTintColor: iconInactiveColor,
-        tabBarLabelStyle: themed($tabBarLabel),
-        tabBarItemStyle: themed($tabBarItem),
-        tabBarLabelPosition: "below-icon",
-        // tabBarPosition: 'left',
-        // animation: 'shift',
-        // tabBarBackground: () => <View />,
-        tabBarVariant: "uikit",
-        tabBarBackground: () => (
-          <View
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0,
-              borderTopColor: theme.colors.separator,
-              // borderTopWidth: 1,
-              overflow: "hidden",
-            }}>
-            <LinearGradient
-              colors={[theme.colors.backgroundStart, theme.colors.backgroundEnd]}
-              style={{
-                position: "absolute",
-                left: 0,
-                right: 0,
-                top: 0,
-                bottom: 0,
-              }}
-              start={{x: 0, y: 0}}
-              end={{x: 0, y: 2}}
-            />
-          </View>
-        ),
-      }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          href: "/home",
-          headerShown: false,
-          // tabBarIcon: ({focused, color}) => (
-          //   <HomeIcon size={28} color={focused ? iconFocusedColor : theme.colors.tabBarIconInactive} />
-          // ),
-          tabBarIcon: ({focused}) => {
-            const mColor = focused ? iconFocusedColor : iconInactiveColor
-            return (
-              <TouchableOpacity onPress={() => replace("/home")}>
-                <HomeIcon size={28} color={mColor} />
-              </TouchableOpacity>
-            )
-          },
-          tabBarLabel: translate("navigation:home"),
-        }}
-      />
-      <Tabs.Screen
-        name="glasses"
-        options={{
-          href: "/glasses",
-          headerShown: false,
-          tabBarIcon: ({focused}) => {
-            const mColor = focused ? iconFocusedColor : iconInactiveColor
-            return <SolarLineIconsSet4 size={28} color={mColor} />
-          },
-          tabBarLabel: translate("navigation:glasses"),
-        }}
-      />
-      {/* <Tabs.Screen
-        name="mirror"
-        options={{
-          href: "/mirror",
-          headerShown: false,
-          tabBarIcon: ({focused, color}) => (
-            <MirrorIcon size={28} color={focused ? iconFocusedColor : theme.colors.textDim} />
-          ),
-          tabBarLabel: translate("navigation:mirror"),
-        }}
-      /> */}
-      <Tabs.Screen
-        name="store"
-        options={{
-          href: "/store",
-          headerShown: false,
-          tabBarIcon: ({focused}) => {
-            const mColor = focused ? iconFocusedColor : iconInactiveColor
-            return <StoreIcon size={28} color={mColor} />
-          },
-          tabBarLabel: translate("navigation:store"),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          href: "/settings",
-          headerShown: false,
-          tabBarIcon: ({focused}) => {
-            const mColor = focused ? iconFocusedColor : iconInactiveColor
-            return <UserIcon size={28} color={mColor} />
-          },
-          tabBarLabel: translate("navigation:account"),
-          // tabBarButton: ({, color}) => userIcon(focused),
-        }}
-      />
+    <Tabs>
+      <TabSlot />
+      <TabList style={themed($tabList)}>
+        <TabTrigger name="home" href="/home" style={themed($tabTrigger)} asChild>
+          <TabButton icon={HomeIcon} label={translate("navigation:home")} />
+        </TabTrigger>
+        <TabTrigger name="glasses" href="/glasses" style={themed($tabTrigger)} asChild>
+          <TabButton icon={SolarLineIconsSet4} label={translate("navigation:glasses")} />
+        </TabTrigger>
+        <TabTrigger name="store" href="/store" style={themed($tabTrigger)} asChild>
+          <TabButton icon={StoreIcon} label={translate("navigation:store")} />
+        </TabTrigger>
+        <TabTrigger name="settings" href="/settings" style={themed($tabTrigger)} asChild>
+          <TabButton icon={UserIcon} label={translate("navigation:account")} />
+        </TabTrigger>
+      </TabList>
     </Tabs>
   )
 }
 
-const $tabBar: ThemedStyle<ViewStyle> = ({colors, spacing}) => ({
-  // backgroundColor: "red",
-  // borderTopColor: colors.separator,
-  // borderTopWidth: 10,
-  // paddingTop: spacing.sm,
-  // height: 40,
-  // width: 50,
+const $tabList: ThemedStyle<ViewStyle> = ({colors, spacing}) => ({
+  flexDirection: "row",
+  backgroundColor: colors.backgroundStart + "cc",
+  position: "absolute",
+  bottom: 0,
+  borderTopColor: colors.separator,
+  paddingVertical: spacing.xs,
+  paddingHorizontal: spacing.sm,
 })
 
-const $tabBarItem: ThemedStyle<ViewStyle> = ({spacing}) => ({
-  // paddingTop: spacing.sm,
-  // paddingBottom: spacing.xs,
+const $tabTrigger: ThemedStyle<ViewStyle> = ({spacing}) => ({
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+  paddingVertical: spacing.xs,
 })
 
-const $tabBarLabel: ThemedStyle<TextStyle> = ({typography}) => ({
+const $tabLabel: ThemedStyle<TextStyle> = ({typography}) => ({
   fontSize: 12,
   fontFamily: typography.primary.medium,
-  lineHeight: 16,
-  marginTop: 4,
+})
+
+const $tabButton: ThemedStyle<ViewStyle> = ({spacing}) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexDirection: "column",
+  gap: spacing.xxs,
+  flex: 1,
 })
