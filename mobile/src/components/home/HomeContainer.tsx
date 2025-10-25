060d4fa17a945df13b7b0a1cf5c7b2e68f187d76
Alex Israelov <alex1115alex@gmail.com>
2025-10-24 21:43:42 -0700

    fix android foreground service in refactor

--- mobile/src/components/home/HomeContainer.tsx
+++ mobile/src/components/home/HomeContainer.tsx
@@ -8,7 +8,7 @@ import {IncompatibleApps} from "@/components/home/IncompatibleApps"
 import {Spacer} from "@/components/ui/Spacer"
 import {useAppTheme} from "@/utils/useAppTheme"
 import {SETTINGS_KEYS, useSetting} from "@/stores/settings"
-import {getModelCapabilities, Capabilities} from "@/../../cloud/packages/types/src"
+import {getModelCapabilities, Capabilities, DeviceTypes} from "@/../../cloud/packages/types/src"
 import ConnectedSimulatedGlassesInfo from "@/components/mirror/ConnectedSimulatedGlassesInfo"
 import {useCoreStatus} from "@/contexts/CoreStatusProvider"
 
@@ -19,11 +19,12 @@ export const HomeContainer: React.FC = () => {
   const {status} = useCoreStatus()
   const features: Capabilities = getModelCapabilities(defaultWearable)
   const connected = status.glasses_info?.model_name
+  const isSimulated = defaultWearable === DeviceTypes.SIMULATED
 
   return (
     <View>
       <CompactDeviceStatus />
-      {connected && features?.hasDisplay && <ConnectedSimulatedGlassesInfo />}
+      {connected && features?.hasDisplay && isSimulated && <ConnectedSimulatedGlassesInfo />}
       <Spacer height={theme.spacing.xs} />
       <ActiveForegroundApp />
       <Spacer height={theme.spacing.xs} />
