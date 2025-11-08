8b170d4a7d94f8039f0191bcd7cbfcc47a7b5330
fossephate <6175367+fossephate@users.noreply.github.com>
2025-11-07 16:37:23 -0800

    cleanup

--- mobile/src/theme/spacing.ts
+++ mobile/src/theme/spacing.ts
@@ -2,16 +2,6 @@
   Use these spacings for margins/paddings and other whitespace throughout your app.
  */
 export const spacing = {
-  "xxxs": 2,
-  "xxs": 4,
-  "xs": 8,
-  "sm": 12,
-  "md": 16,
-  "lg": 24,
-  "xl": 32,
-  "xxl": 48,
-  "xxxl": 64,
-
   s0: 0,
   s0_5: 2,
   s1: 4,
