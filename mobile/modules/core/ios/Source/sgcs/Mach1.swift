9f01cd3658d4e6b781e636bf6162ed66476691ba
Alex Israelov <alex1115alex@gmail.com>
2025-11-10 22:47:33 -0800

    fix ios issue

--- mobile/modules/core/ios/Source/sgcs/Mach1.swift
+++ mobile/modules/core/ios/Source/sgcs/Mach1.swift
@@ -111,7 +111,10 @@ class Mach1: UltraliteBaseViewController, SGCManager {
 
     func cleanup() {}
 
-    let type = DeviceTypes.MACH1
+    var type: String {
+        return DeviceTypes.MACH1
+    }
+
     let hasMic: Bool = false
     var caseOpen = false
     var caseRemoved = true
