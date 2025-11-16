0a22bb3dd96ac1af2eb642a574d42a0551860264
Alex Israelov <alex1115alex@gmail.com>
2025-11-15 19:15:44 -0800

    fix android script

--- mobile/scripts/android.mjs
+++ mobile/scripts/android.mjs
@@ -1,8 +1,11 @@
 #!/usr/bin/env zx
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { setBuildEnv } from './set-build-env.mjs';
 
 // Configure zx to run from project root
-$.cwd = path.resolve(import.meta.dirname, '..');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+$.cwd = path.resolve(__dirname, '..');
 
 // Set build environment variables
 await setBuildEnv();
