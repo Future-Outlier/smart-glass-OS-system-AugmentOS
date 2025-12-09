2775c7352e2c500ae2013f2969f2ca31b20ec62d
fossephate <6175367+fossephate@users.noreply.github.com>
2025-12-09 12:26:17 -0800

    fix-home-icon

--- mobile/src/components/icons/HomeIcon.tsx
+++ mobile/src/components/icons/HomeIcon.tsx
@@ -12,7 +12,7 @@ interface HomeIconProps extends Omit<ViewProps, "style"> {
   fill?: string
 }
 
-export function HomeIcon({size = 24, color, containerStyle, fill, ...viewProps}: HomeIconProps) {
+export function HomeIcon({size = 24, containerStyle, fill, ...viewProps}: HomeIconProps) {
   const {theme} = useAppTheme()
 
   if (fill) {
@@ -26,7 +26,7 @@ export function HomeIcon({size = 24, color, containerStyle, fill, ...viewProps}:
           <Path d="M9 22V12H15V22" fill={theme.colors.primary_foreground} />
           <Path
             d="M9 22V12H15V22M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z"
-            stroke={theme.colors.primary_foreground}
+            stroke={theme.colors.primary}
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
