3b18ab2a6fbbf168b9666673beb63a030431e1b9
fossephate <6175367+fossephate@users.noreply.github.com>
2025-12-17 11:01:31 -0800

    save

--- mobile/src/app/onboarding/live/start.tsx
+++ mobile/src/app/onboarding/live/start.tsx
@@ -16,6 +16,7 @@ interface OnboardingVideo {
   transition: boolean
   title?: string
   subtitle?: string
+  subtitle2?: string
   info?: string
 }
 
@@ -59,6 +60,15 @@ const videoFiles: OnboardingVideo[] = [
     info: translate("onboarding:liveLedFlashWarning"),
   },
   {
+    source: require("../../../../assets/onboarding/live/ONB5_action_button_record.mp4"),
+    name: "Action Button Stop Recording",
+    loop: true,
+    transition: false,
+    title: translate("onboarding:liveStopRecording"),
+    subtitle: translate("onboarding:livePressAndHoldAgain"),
+    info: translate("onboarding:liveLedFlashWarning"),
+  },
+  {
     source: require("../../../../assets/onboarding/live/ONB6_transition_trackpad.mp4"),
     name: "Transition Trackpad",
     loop: false,
@@ -69,18 +79,23 @@ const videoFiles: OnboardingVideo[] = [
     name: "Trackpad Tap",
     loop: true,
     transition: false,
+    title: translate("onboarding:livePlayMusic"),
+    subtitle: translate("onboarding:liveDoubleTapTouchpad"),
   },
-  {
-    source: require("../../../../assets/onboarding/live/ONB8_transition_trackpad2.mp4"),
-    name: "Transition Trackpad 2",
-    loop: false,
-    transition: true,
-  },
+  // {
+  //   source: require("../../../../assets/onboarding/live/ONB8_transition_trackpad2.mp4"),
+  //   name: "Transition Trackpad 2",
+  //   loop: false,
+  //   transition: true,
+  // },
   {
     source: require("../../../../assets/onboarding/live/ONB8_trackpad_slide.mp4"),
-    name: "Trackpad Slide",
+    name: "Trackpad Volume Slide",
     loop: true,
     transition: false,
+    title: translate("onboarding:liveAdjustVolume"),
+    subtitle: translate("onboarding:liveSwipeTouchpadUp"),
+    subtitle2: translate("onboarding:liveSwipeTouchpadDown"),
   },
   {
     source: require("../../../../assets/onboarding/live/ONB9_trackpad_pause.mp4"),
@@ -105,50 +120,87 @@ const videoFiles: OnboardingVideo[] = [
 export default function Onboarding1() {
   const {goBack} = useNavigationHistory()
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [secondaryIndex, setSecondaryIndex] = useState(1)
   const [showNextButton, setShowNextButton] = useState(false)
   const [showReplayButton, setShowReplayButton] = useState(false)
   const [hasStarted, setHasStarted] = useState(false)
+  const [playCount, setPlayCount] = useState(0)
 
   const player: VideoPlayer = useVideoPlayer(videoFiles[currentIndex].source, (player: any) => {
-    // player.loop = videoFiles[currentIndex].loop
     player.loop = false
   })
+  const player2: VideoPlayer = useVideoPlayer(videoFiles[secondaryIndex].source, (player2: any) => {
+    player2.loop = false
+  })
+  const [currentPlayer, setCurrentPlayer] = useState(player)
 
   const counter = `${currentIndex + 1} / ${videoFiles.length}`
   const video = videoFiles[currentIndex]
 
   const handleNext = useCallback(() => {
-    // const handleNext = () => {
     const nextIndex = currentIndex < videoFiles.length - 1 ? currentIndex + 1 : 0
     setShowNextButton(false)
     setShowReplayButton(false)
+
+    // special case for when the video files are the same:
+    if (videoFiles[nextIndex].source == videoFiles[currentIndex].source) {
+      // we didn't change the video, so we need to reset the current time
+      player.currentTime = 0
+      player.play()
+      setCurrentIndex(nextIndex)
+      setPlayCount(0)
+      return
+    }
+
     setCurrentIndex(nextIndex)
+    setPlayCount(0)
     player.replace(videoFiles[nextIndex].source)
-    // player.loop = false
-    // player.loop = videoFiles[nextIndex].loop
-    // player.loop = true
-    player.play()
   }, [currentIndex, player])
 
   useEffect(() => {
+    const subscription = player.addListener("statusChange", (status: any) => {
+      console.log("statusChange", status)
+      if (currentIndex === 0) {
+        // we don't auto play the first video
+        return
+      }
+      if (status.status === "readyToPlay") {
+        player.play()
+      }
+    })
+
+    return () => subscription.remove()
+  }, [player])
+
+  useEffect(() => {
     const subscription = player.addListener("playingChange", (status: any) => {
+      console.log("playingChange", status)
       if (!status.isPlaying && player.currentTime >= player.duration - 0.1) {
         if (video.transition) {
           handleNext()
           return
         }
-        setShowReplayButton(true)
-        setShowNextButton(true)
+        if (playCount < 1) {
+          // Play again
+          setPlayCount(prev => prev + 1)
+          player.currentTime = 0
+          player.play()
+        } else {
+          // Played twice, now stop
+          setShowReplayButton(true)
+          setShowNextButton(true)
+        }
       }
     })
 
     return () => {
       subscription.remove()
     }
-  }, [player, video, handleNext])
+  }, [player, video, handleNext, playCount])
 
   const handleReplay = useCallback(() => {
     setShowReplayButton(false)
+    setPlayCount(0)
     player.currentTime = 0
     player.play()
   }, [player])
@@ -184,7 +236,8 @@ export default function Onboarding1() {
 
           <View className="-mx-6">
             <View className="relative">
-              <VideoView player={player} style={{width: "100%", aspectRatio: 1}} nativeControls={false} />
+              <VideoView player={currentPlayer} style={{width: "100%", aspectRatio: 1}} nativeControls={false} />
+              
 
               {showReplayButton && (
                 <View className="absolute bottom-8 left-0 right-0 items-center">
@@ -197,6 +250,7 @@ export default function Onboarding1() {
           {hasStarted && (
             <View className="flex flex-col gap-2 mt-4">
               {video.subtitle && <Text className="text-center text-xl font-semibold" text={video.subtitle} />}
+              {video.subtitle2 && <Text className="text-center text-xl font-semibold" text={video.subtitle2} />}
               {video.info && (
                 <View className="flex flex-row gap-2">
                   <Icon name="info" size={20} color="muted-foreground" />
@@ -218,7 +272,7 @@ export default function Onboarding1() {
           )}
 
           {!hasStarted && (
-            <View className="flex flex-col gap-2 mt-8">
+            <View className="flex flex-col gap-4 mt-8">
               <Button flexContainer tx="onboarding:continueOnboarding" onPress={handleStart} />
               <Button
                 flexContainer
