72c9c4d681697942e8de135ff922e3d5ed89af46
fossephate <6175367+fossephate@users.noreply.github.com>
2025-12-17 17:29:04 -0800

    works

--- mobile/src/app/onboarding/os.tsx
+++ mobile/src/app/onboarding/os.tsx
@@ -1,36 +1,18 @@
-// import {Image} from "expo-image"
-// import {useNavigationHistory} from "@/contexts/NavigationHistoryContext"
-// import {useAppTheme} from "@/contexts/ThemeContext"
-// import {View} from "react-native"
-
 import {Screen} from "@/components/ignite"
-import {translate} from "@/i18n"
 import {OnboardingGuide, OnboardingStep} from "@/components/onboarding/OnboardingGuide"
+import {translate} from "@/i18n"
 import {spacing} from "@/theme"
 
 // NOTE: you can't have 2 transition videos in a row or things will break:
 const steps: OnboardingStep[] = [
   {
     type: "image",
+    name: "Start Onboarding",
     source: require("@assets/glasses/g1.png"),
     imageContainerStyle: {
       paddingHorizontal: spacing.s6,
     },
-    // imageComponent: () => {
-    //   return (
-    //     <View className="">
-    //       <Image
-    //         source={require("@assets/glasses/g1.png")}
-    //         style={{width: "100%", height: "100%"}}
-    //         contentFit="contain"
-    //       />
-    //     </View>
-    //   )
-    // },
-    name: "Start Onboarding",
     transition: false,
-    // title: "Welcome to Mentra Live",
-    // info: "Learn the basics",
     bullets: [
       translate("onboarding:osStartStopApps"),
       translate("onboarding:osStartStopAppsBullet1"),
@@ -39,21 +21,55 @@ const steps: OnboardingStep[] = [
   },
   {
     type: "video",
+    name: "Action Button Click",
     source: require("@assets/onboarding/live/ONB4_action_button_click.mp4"),
-    name: "Start Onboarding",
     playCount: 1,
     transition: false,
-    // title: "Welcome to Mentra Live",
-    // info: "Learn the basics",
+    bullets: [
+      translate("onboarding:osStartStopApps"),
+      translate("onboarding:osStartStopAppsBullet1"),
+      translate("onboarding:osStartStopAppsBullet2"),
+    ],
   },
   {
-    type: "video",
-    source: require("@assets/onboarding/live/ONB7_trackpad_tap.mp4"),
-    name: "Start Onboarding",
-    playCount: 1,
+    type: "image",
+    name: "Unknown Wearable",
     transition: false,
-    // title: "Welcome to Mentra Live",
-    // info: "Learn the basics",
+    source: require("@assets/glasses/unknown_wearable.png"),
+    imageContainerStyle: {
+      paddingHorizontal: spacing.s6,
+    },
+    bullets: [
+      translate("onboarding:osStartStopApps"),
+      translate("onboarding:osStartStopAppsBullet1"),
+      translate("onboarding:osStartStopAppsBullet2"),
+    ],
+  },
+  {
+    name: "Action Button Click",
+    type: "video",
+    transition: false,
+    playCount: 1,
+    source: require("@assets/onboarding/live/ONB4_action_button_click.mp4"),
+    bullets: [
+      translate("onboarding:osStartStopApps"),
+      translate("onboarding:osStartStopAppsBullet1"),
+      translate("onboarding:osStartStopAppsBullet2"),
+    ],
+  },
+  {
+    name: "Unknown Wearable",
+    type: "image",
+    transition: false,
+    source: require("@assets/glasses/unknown_wearable.png"),
+    imageContainerStyle: {
+      paddingHorizontal: spacing.s6,
+    },
+    bullets: [
+      translate("onboarding:osStartStopApps"),
+      translate("onboarding:osStartStopAppsBullet1"),
+      translate("onboarding:osStartStopAppsBullet2"),
+    ],
   },
 ]
 
