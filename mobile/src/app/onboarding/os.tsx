57809b05b19fe3e7d34c6e7202dcbbeaa7436e72
fossephate <6175367+fossephate@users.noreply.github.com>
2025-12-22 15:17:55 -0800

    disable-steps-too-big

--- mobile/src/app/onboarding/os.tsx
+++ mobile/src/app/onboarding/os.tsx
@@ -1,9 +1,8 @@
 import {Screen} from "@/components/ignite"
 import {OnboardingGuide, OnboardingStep} from "@/components/onboarding/OnboardingGuide"
-import { useNavigationHistory } from "@/contexts/NavigationHistoryContext"
+import {useNavigationHistory} from "@/contexts/NavigationHistoryContext"
 import {translate} from "@/i18n"
-import { SETTINGS, useSetting } from "@/stores/settings"
-import {spacing} from "@/theme"
+import {SETTINGS, useSetting} from "@/stores/settings"
 
 // NOTE: you can't have 2 transition videos in a row or things will break:
 const steps: OnboardingStep[] = [
@@ -46,32 +45,32 @@ const steps: OnboardingStep[] = [
       translate("onboarding:osBackgroundAppsBullet2"),
     ],
   },
-  {
-    type: "video",
-    name: "Foreground and Background Apps",
-    source: require("@assets/onboarding/os/foreground_background_apps.mov"),
-    containerClassName: "bg-background",
-    transition: false,
-    playCount: 2,
-    bullets: [
-      translate("onboarding:osForegroundAndBackgroundApps"),
-      translate("onboarding:osForegroundAndBackgroundAppsBullet1"),
-      translate("onboarding:osForegroundAndBackgroundAppsBullet2"),
-    ],
-  },
-  {
-    type: "video",
-    name: "Mentra AI",
-    source: require("@assets/onboarding/os/mentra_ai.mov"),
-    containerClassName: "bg-background",
-    transition: false,
-    playCount: 2,
-    bullets: [
-      translate("onboarding:osMentraAi"),
-      translate("onboarding:osMentraAiBullet1"),
-      translate("onboarding:osMentraAiBullet2"),
-    ],
-  },
+  // {
+  //   type: "video",
+  //   name: "Foreground and Background Apps",
+  //   source: require("@assets/onboarding/os/foreground_background_apps.mov"),
+  //   containerClassName: "bg-background",
+  //   transition: false,
+  //   playCount: 2,
+  //   bullets: [
+  //     translate("onboarding:osForegroundAndBackgroundApps"),
+  //     translate("onboarding:osForegroundAndBackgroundAppsBullet1"),
+  //     translate("onboarding:osForegroundAndBackgroundAppsBullet2"),
+  //   ],
+  // },
+  // {
+  //   type: "video",
+  //   name: "Mentra AI",
+  //   source: require("@assets/onboarding/os/mentra_ai.mov"),
+  //   containerClassName: "bg-background",
+  //   transition: false,
+  //   playCount: 2,
+  //   bullets: [
+  //     translate("onboarding:osMentraAi"),
+  //     translate("onboarding:osMentraAiBullet1"),
+  //     translate("onboarding:osMentraAiBullet2"),
+  //   ],
+  // },
 ]
 
 export default function MentraOSOnboarding() {
@@ -79,10 +78,15 @@ export default function MentraOSOnboarding() {
   const [_onboardingOsCompleted, setOnboardingOsCompleted] = useSetting(SETTINGS.onboarding_os_completed.key)
   return (
     <Screen preset="fixed" safeAreaEdges={["bottom"]}>
-      <OnboardingGuide steps={steps} autoStart={true} showSkipButton={false} endButtonFn={() => {
-        setOnboardingOsCompleted(true)
-        clearHistoryAndGoHome()
-      }}/>
+      <OnboardingGuide
+        steps={steps}
+        autoStart={true}
+        showSkipButton={false}
+        endButtonFn={() => {
+          setOnboardingOsCompleted(true)
+          clearHistoryAndGoHome()
+        }}
+      />
     </Screen>
   )
 }
