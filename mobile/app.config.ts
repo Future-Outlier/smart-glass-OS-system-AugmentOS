eb9b63515f50488dca736fe6a3e8ae902fb4d0b7
Alex Israelov <alex1115alex@gmail.com>
2026-01-22 14:29:48 -0800

    bump ver

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -26,7 +26,7 @@ module.exports = ({config}: ConfigContext): Partial<ExpoConfig> => {
     android: {
       icon: "./assets/app-icons/ic_launcher.png",
       package: "com.mentra.mentra",
-      versionCode: 75,
+      versionCode: 76,
       adaptiveIcon: {
         foregroundImage: "./assets/app-icons/ic_launcher_foreground.png",
         backgroundImage: "./assets/app-icons/ic_launcher.png",
