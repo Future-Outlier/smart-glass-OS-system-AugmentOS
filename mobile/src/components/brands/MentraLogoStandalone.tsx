d651455dc1f87796dde7f60f744c221b18e75dbd
fossephate <6175367+fossephate@users.noreply.github.com>
2026-02-18 10:31:35 -0800

    logo-color

--- mobile/src/components/brands/MentraLogoStandalone.tsx
+++ mobile/src/components/brands/MentraLogoStandalone.tsx
@@ -7,7 +7,7 @@ interface LogoProps extends SvgProps {
   fill?: string
 }
 
-export const MentraLogoStandalone: React.FC<LogoProps> = ({width = 33, height = 16, fill = "#00B869", ...props}) => (
+export const MentraLogoStandalone: React.FC<LogoProps> = ({width = 33, height = 16, fill = "#36DD89", ...props}) => (
   <Svg width={width} height={height} viewBox="0 0 33 16" fill="none" {...props} preserveAspectRatio="xMidYMid meet">
     <Rect y={8.88867} width={7.81818} height={7.11111} fill={fill} />
     <Path d="M6.18182 0L20.2727 8.88889V16L6.18182 7.11111V0Z" fill={fill} />
