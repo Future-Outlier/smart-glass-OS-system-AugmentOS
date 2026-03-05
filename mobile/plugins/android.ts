f35c8c5e913b77c351ccd330743f2270a6ea596c
Alex Israelov <alex1115alex@gmail.com>
2026-03-04 22:47:00 -0800

    bump to 2p7p0

--- mobile/plugins/android.ts
+++ mobile/plugins/android.ts
@@ -113,8 +113,8 @@ if (project.hasProperty("sentryUploadEnabled") && project.property("sentryUpload
       )
     }
 
-    // 2. Update versionName to 2.6.0
-    buildGradle = buildGradle.replace(/versionName\s+["'][^"']*["']/, 'versionName "2.6.0"')
+    // 2. Update versionName to 2.7.0
+    buildGradle = buildGradle.replace(/versionName\s+["'][^"']*["']/, 'versionName "2.7.0"')
 
     // 3. Add externalNativeBuild configuration in defaultConfig
     if (!buildGradle.includes("externalNativeBuild")) {
